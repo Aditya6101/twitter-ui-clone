1a61ce6f8b20174b38499f037697217630acef47
Aditya Kamble <adityakamble6101@gmail.com>
2022-03-08 13:35:22 +0530

    styles modified.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,8 +15,8 @@ const Layout: FC<Props> = ({ children }) => {
   return (
     <div className="relative flex flex-col h-screen">
       <Header />
-      <main className="flex-1">{children}</main>
-      <button className="absolute p-3 rounded-full bg-twitter-blue bottom-20 right-4">
+      <main className="flex-1 overflow-y-scroll">{children}</main>
+      <button className="fixed p-3 rounded-full shadow bg-twitter-blue bottom-20 right-4">
         {pathname === '/messages' ? (
           <MessageIcon className="p-px text-white h-7 w-7" />
         ) : (
