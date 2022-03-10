7b9d1f231ef03f06205abd34de213efe89e2f5b9
Aditya Kamble <adityakamble6101@gmail.com>
2022-03-10 14:37:46 +0530

    stats added to each tweet icons.

--- components/IconBar.tsx
+++ components/IconBar.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import { FC } from 'react';
 import Like from './icons/Like';
 import Options from './icons/Options';
 import Reply from './icons/Reply';
 import RT from './icons/RT';
 
-const IconBar = () => {
+type Props = {
+  replies: number;
+  rts: number;
+  likes: number;
+};
+
+const IconBar: FC<Props> = ({ replies, rts, likes }) => {
   return (
-    <div className="flex items-center mt-3 justify-between">
-      <Reply className="h-5 w-5 text-gray-500" />
-      <RT className="h-5 w-5 text-gray-500" />
-      <Like className="h-5 w-5 text-gray-500" />
-      <Options className="h-5 w-5 text-gray-500" />
+    <div className="flex items-center mt-3 justify-between text-gray-500">
+      <span className="flex items-center">
+        <Reply className="h-5 w-5 mr-1" />
+        <p>{replies}</p>
+      </span>
+      <span className="flex items-center">
+        <RT className="h-5 w-5 mr-1" />
+        <p>{rts}</p>
+      </span>
+      <span className="flex items-center">
+        <Like className="h-5 w-5 mr-1" />
+        <p>{likes}</p>
+      </span>
+      <Options className="h-5 w-5 mr-1" />
     </div>
   );
 };
